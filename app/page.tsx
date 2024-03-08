2c12dc09773a9d373291a79dc3ba532446f1c05d
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-03-08 08:56:57 -0500

    test custom Button > variant

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-    return <p className="text-red-500 font-bold">Hello</p>;
+    return (
+        <div>
+            <Button variant="purple" size="lg">
+                Delete
+            </Button>
+        </div>
+    );
 }
