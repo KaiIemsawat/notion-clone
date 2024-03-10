d8c5a30185644579dac517f3428a57adff651867
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-03-10 09:08:03 -0400

    base protected route

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ReactNode } from "react";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+
+import { Spinner } from "@/components/spinner";
+
+const MainLayout = ({ children }: { children: ReactNode }) => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
+    if (isLoading) {
+        return (
+            <div className="h-full flex items-center justify-center">
+                <Spinner size="lg" />
+            </div>
+        );
+    }
+
+    if (!isAuthenticated) {
+        return redirect("/");
+    }
+
+    return <div>{children}</div>;
+};
+export default MainLayout;
