16deadf4b884605583dd2ed9a26118abc3630e74
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-03-13 21:33:14 -0400

    display pages/notes

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -12,16 +12,20 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useMutation, useQuery } from "convex/react";
+import {
+    useMutation,
+    // useQuery
+} from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
 
-    const documents = useQuery(api.documents.get);
+    // const documents = useQuery(api.documents.get);
     const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
@@ -180,9 +184,10 @@ export const Navigation = () => {
                     />
                 </div>
                 <div className="mt-4">
-                    {documents?.map((document) => (
+                    {/* {documents?.map((document) => (
                         <p key={document._id}>{document.title}</p>
-                    ))}
+                    ))} */}
+                    <DocumentList />
                 </div>
 
                 {/* Hover over sidebar */}
