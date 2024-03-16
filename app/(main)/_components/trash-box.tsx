9cf75fc59abc6661d94adb90de4d4f2985d20271
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-03-15 20:52:10 -0400

    Confirm delete

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -9,6 +9,7 @@ import { toast } from "sonner";
 import { Spinner } from "@/components/spinner";
 import { Search, Trash, Undo } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 
 export const TrashBox = () => {
     const router = useRouter();
@@ -103,12 +104,16 @@ export const TrashBox = () => {
                             >
                                 <Undo className="h-4 w-4 text-muted-foreground" />
                             </div>
-                            <div
-                                className="rounded-sm p-2 hover:bg-neutral-200"
-                                role="button"
+                            <ConfirmModal
+                                onConfirm={() => onRemove(document._id)}
                             >
-                                <Trash className="h-4 w-4 text-muted-foreground" />
-                            </div>
+                                <div
+                                    className="rounded-sm p-2 hover:bg-neutral-200"
+                                    role="button"
+                                >
+                                    <Trash className="h-4 w-4 text-muted-foreground" />
+                                </div>
+                            </ConfirmModal>
                         </div>
                     </div>
                 ))}
